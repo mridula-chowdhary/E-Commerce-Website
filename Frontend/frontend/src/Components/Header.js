dd96aa3c82959bd7b6382cee92f605d66c679fcf
Mridula Chowdhary <mridulachowdhary21@gmail.com>
2023-08-03 10:23:21 +0530

    Built add-product api and tested

--- Frontend/frontend/src/Components/Header.js
+++ Frontend/frontend/src/Components/Header.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import React from 'react';
 import {Link,useNavigate} from 'react-router-dom';
 
@@ -10,18 +11,22 @@ const Header=()=>{
     }
     return (
         <div>
+            <img className="logo" src ="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" alt='logo'></img>
+            {auth ? 
          <ul className='Header-ul'>
          <li> <Link to="/">Products </Link> </li>
          <li> <Link to="/add">Add Product </Link> </li>
          <li> <Link to="/update">Update product </Link> </li>
          <li> <Link to="/profile">Profile </Link> </li>
-         <li> {auth ? <Link onClick={logout} to ="/signup">Logout </Link>: 
-         <Link to="/signup"> SignUp </Link> }</li>
-         <li> <Link to ="/login"> Login </Link></li>
-
-
- 
+         <li><Link onClick={logout} to ="/signup">Logout ({JSON.parse(auth).name})</Link></li>
          </ul>
+            : 
+        <ul className='Header-ul nav-right' >
+             <li> <Link to ="/login"> Login </Link></li> 
+            <li> <Link to="/signup"> SignUp </Link> </li>
+            </ul>
+        }
+
         </div>
     )
 }
--- Frontend/frontend/src/Components/Login.js
+++ Frontend/frontend/src/Components/Login.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useNavigate} from 'react-router-dom'
 const Login=()=>{
     const [email,setEmail] = React.useState('');
     const [password,setPassword]=React.useState('');
-    const handleLogin=()=>{
-        console.warn(email,password);
+    const navigate = useNavigate();
+    useEffect(()=>{
+        const auth =localStorage.getItem('user');
+        if(auth){
+          navigate('/')
+        }
+       })
+    const handleLogin=async()=>{
+        console.warn(email,password)
+        let result = await fetch('http://localhost:5000/login',{
+            method:"post",
+            body:JSON.stringify({email,password}),
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+        });
+        result = await result.json();
+        console.warn(result)
+        if(result.name){
+            localStorage.setItem('user',JSON.stringify(result));
+            navigate("/")
+        }
+        else{
+            alert("Please Enter Correct Info")
+        }
     }
    return (
     <div>
        <div className='login'>
+        <h2>Login</h2>
         <input type = "text" className='inputBox' placeholder='Enter Email' onChange={(e)=>setEmail(e.target.value)}></input>
         <input type = "password" className='inputBox' placeholder='Enter Password'  onChange={(e)=>setPassword(e.target.value)}></input>
         <button type = "button" className='signButton' onClick={handleLogin}> Login</button>
